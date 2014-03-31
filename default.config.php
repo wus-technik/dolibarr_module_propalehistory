cf48c489aa320b2662b3e40b0ba1094015e85a8c
Alexis Algoud <alexis@atm-consulting.fr>
2014-03-31 17:18:29 +0200

    correction pour usage core en module

--- default.config.php
+++ default.config.php
@@ -12,13 +12,14 @@
 	} else {
 		global $dolibarr_main_db_host, $dolibarr_main_db_name, $dolibarr_main_db_user, $dolibarr_main_db_pass;
 	}
+
 	if(!defined('DB_HOST')) {
 		define('DB_HOST',$dolibarr_main_db_host);
 		define('DB_NAME',$dolibarr_main_db_name);
 		define('DB_USER',$dolibarr_main_db_user);
 		define('DB_PASS',$dolibarr_main_db_pass);
 		define('DB_DRIVER',$dolibarr_main_db_type);
-		dol_include_once('/propalehistory/core/inc.core.php');
+		dol_include_once('/coreatm/inc.core.php');
 	
 	}
 	

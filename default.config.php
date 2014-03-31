9cec86566938f7deba65affc3b73261388a0a0b3
Alexis Algoud <alexis@atm-consulting.fr>
2014-03-31 17:22:18 +0200

    debug

--- default.config.php
+++ default.config.php
@@ -5,10 +5,10 @@
 	else $dir = '../../';
 
 	if(!defined('INC_FROM_DOLIBARR') && defined('INC_FROM_CRON_SCRIPT')) {
-		include($dir."master.inc.php");
+		include_once($dir."master.inc.php");
 	}
 	elseif(!defined('INC_FROM_DOLIBARR')) {
-		include($dir."main.inc.php");
+		include_once($dir."main.inc.php");
 	} else {
 		global $dolibarr_main_db_host, $dolibarr_main_db_name, $dolibarr_main_db_user, $dolibarr_main_db_pass;
 	}

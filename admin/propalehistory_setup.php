3544d79682192076351ef59937b5d959a8edc5c0
jfefe <jfefe@aternatik.fr>
2017-03-27 12:50:47 +0200

    New option to create PDF archive or not

--- admin/propalehistory_setup.php
+++ admin/propalehistory_setup.php
@@ -133,6 +133,18 @@ if($ok) {
 	print '</form>';
 	print '</td></tr>';
 
+	$var=!$var;
+	print '<tr '.$bc[$var].'>';
+	print '<td>'.$langs->trans("PROPALEHISTORY_ARCHIVE_PDF_TOO").'</td>';
+	print '<td align="center" width="20">&nbsp;</td>';
+	print '<td align="right" width="300">';
+	print '<form method="POST" action="'.$_SERVER['PHP_SELF'].'">';
+	print '<input type="hidden" name="token" value="'.$_SESSION['newtoken'].'">';
+	print '<input type="hidden" name="action" value="set_PROPALEHISTORY_ARCHIVE_PDF_TOO">';
+	print $form->selectyesno("PROPALEHISTORY_ARCHIVE_PDF_TOO",$conf->global->PROPALEHISTORY_ARCHIVE_PDF_TOO,1);
+	print '<input type="submit" class="button" value="'.$langs->trans("Modify").'">';
+	print '</form>';
+	print '</td></tr>';
 	
 } else {
 	print $langs->trans('ModuleNeedProposalOrOrderModule');

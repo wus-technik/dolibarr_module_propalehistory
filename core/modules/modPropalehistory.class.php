8d7cfcce1a8e0006e13ba7fecbb9f256da50fa08
Atm-Gregr <greg.rastklan@atm-consulting.fr>
2022-07-15 10:58:55 +0200

    changelog + numversion

--- core/modules/modPropalehistory.class.php
+++ core/modules/modPropalehistory.class.php
@@ -63,7 +63,7 @@ class modPropalehistory extends DolibarrModules
         // (where XXX is value of numeric property 'numero' of module)
         $this->description = "Gestion de l'historique des propositions commerciales";
         // Possible values for version are: 'development', 'experimental' or version
-        $this->version = '2.3.2';
+        $this->version = '2.3.3';
 		// Url to the file with your last numberversion of this module
 		require_once __DIR__ . '/../../class/techatm.class.php';
 		$this->url_last_version = \propalehistory\TechATM::getLastModuleVersionUrl($this);

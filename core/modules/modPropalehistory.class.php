58353d3fc88180212f5dddd383ff247c2a8c36c6
Irvine Fleith <irvine.fleith@atm-consulting.fr>
2024-10-14 11:31:45 +0200

    FIX : isModEnabled instead of $conf->propalehistory->enabled

--- core/modules/modPropalehistory.class.php
+++ core/modules/modPropalehistory.class.php
@@ -63,7 +63,7 @@ class modPropalehistory extends DolibarrModules
         // (where XXX is value of numeric property 'numero' of module)
         $this->description = "Gestion de l'historique des propositions commerciales";
         // Possible values for version are: 'development', 'experimental' or version
-        $this->version = '2.6.2';
+        $this->version = '2.6.3';
 		// Url to the file with your last numberversion of this module
 		require_once __DIR__ . '/../../class/techatm.class.php';
 		$this->url_last_version = \propalehistory\TechATM::getLastModuleVersionUrl($this);
@@ -211,7 +211,7 @@ class modPropalehistory extends DolibarrModules
         include_once DOL_DOCUMENT_ROOT.'/core/class/extrafields.class.php';
         $extrafields = new ExtraFields($this->db);
         // proposal version
-        $result = $extrafields->addExtraField('propalehistory_version_num', 'PropaleHistoryVersionNum', 'int', 1000, '', 'propal', 0, 0, '', '', 0, '', '-4', '', '', 0, 'propalehistory@propalehistory', '$conf->propalehistory->enabled', 0);
+        $result = $extrafields->addExtraField('propalehistory_version_num', 'PropaleHistoryVersionNum', 'int', 1000, '', 'propal', 0, 0, '', '', 0, '', '-4', '', '', 0, 'propalehistory@propalehistory', 'isModEnabled("propalehistory")', 0);
 
         return $this->_init($sql, $options);
     }
